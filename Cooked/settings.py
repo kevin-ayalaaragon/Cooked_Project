9249f9ee5a459a8172c84dc626656cdc02d97263
kevin-ayalaaragon <kayalaaragon@gmail.com>
2026-02-25 14:05:24 -0500

    Update allowed hosts for deployment

--- Cooked/settings.py
+++ Cooked/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-%8^56(b4zcu^&eju*#!b9x9=0q*+d_3k#3@p+o@a67gosj+*5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
