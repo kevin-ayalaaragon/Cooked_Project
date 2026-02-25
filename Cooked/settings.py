39b03ddb725e45589b2c7e4aebea94979401bd27
kevin-ayalaaragon <kayalaaragon@gmail.com>
2026-02-25 17:32:48 -0500

    Updated to permanent host

--- Cooked/settings.py
+++ Cooked/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-%8^56(b4zcu^&eju*#!b9x9=0q*+d_3k#3@p+o@a67gosj+*5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cooked-app-4kof.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
