3624e30f1fa39b780e77a8fabde12ce373be89f4
kevin-ayalaaragon <kayalaaragon@gmail.com>
2026-02-26 11:39:17 -0500

    Update cooked status label with chef emoji

--- recipes/models.py
+++ recipes/models.py
@@ -27,6 +27,9 @@ class Review(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField()
-    cooked_status = models.BooleanField(default=False)
+    cooked_status = models.BooleanField(
+        default=False, 
+        verbose_name="I cooked 👨‍🍳"  # Update this line
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     
